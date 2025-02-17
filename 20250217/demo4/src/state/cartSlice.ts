84315a2025c0ae307178ca3dca6d3b2354f753cf
Gilberto Bermúdez Garro <gbermude@outlook.com>
2025-02-17 13:16:55 -0600

    Update

---
+++ 20250217/demo4/src/state/cartSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import IProduct from "../models/IProduct";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: Array<IProduct>()
+    },
+    reducers: {
+        addToCart : (state, action) => { 
+            //console.log('addToCart: ', action.type, action.payload)           
+            // Si el producto no está en el carrito se agrega
+            if (state.items.findIndex(p=> p.id === action.payload.id) === -1)
+            {
+                state.items = [action.payload, ...state.items]
+            }
+        },
+        removeFromCart : (state, action) => {
+            state.items = state.items.filter(p => p.id !== action.payload.id)
+        },
+        clearCart : (state) => {
+            state.items = []
+        }
+    }
+})
+
+export default cartSlice
